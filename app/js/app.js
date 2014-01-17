2889925c3cab0fcaf65e38811aa604481b804c27
Eason Pai <mediakid@gmail.com>
2014-01-17 10:47:39 +0800

    step-7-done

--- app/js/app.js
+++ app/js/app.js
@@ -2,18 +2,25 @@
 
 /* App Module */
 
+// app module裡面包含兩個module，包括route
 var phonecatApp = angular.module('phonecatApp', [
   'ngRoute',
   'phonecatControllers'
 ]);
 
+// config module, 拿到route
 phonecatApp.config(['$routeProvider',
   function($routeProvider) {
+      
     $routeProvider.
+    
+    // 指定partials & controller
       when('/phones', {
         templateUrl: 'partials/phone-list.html',
         controller: 'PhoneListCtrl'
       }).
+    
+    // 用:抓到 route property
       when('/phones/:phoneId', {
         templateUrl: 'partials/phone-detail.html',
         controller: 'PhoneDetailCtrl'
